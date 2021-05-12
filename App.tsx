1ee88963447a1252eacf40864f7ff898c1896e74
Aleksander Mikucki <aleksander.mikucki@swmansion.com>
2021-05-12 15:23:34 +0200

    webrtc

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  RTCPeerConnection,
+  RTCIceCandidate,
+  RTCSessionDescription,
+  RTCView,
+  MediaStream,
+  MediaStreamTrack,
+  mediaDevices,
+  registerGlobals,
+} from "react-native-webrtc";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <RTCView streamURL={this.state.stream.toURL()} />
+
       <StatusBar style="auto" />
     </View>
   );
